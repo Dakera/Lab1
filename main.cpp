e454d6fa7bf5ba2a8af4d08b0d6967a0bde40c85
Dakera <3dakera@gmail.com>
2022-04-01 12:37:01 +0700

    Работает
    Ура работает но пока всего мало

--- main.cpp
+++ main.cpp
@@ -3,35 +3,45 @@
 
 using namespace std;
 
-class my_stack : public QObject
+class my_stack //: public QObject
 {
-    Q_OBJECT;
-    my_stack *stack_p; // указатель на стек
+    //Q_OBJECT; // Не знаю почему, но с этим всё ломается
 private:
-    int top; // текущий элемент стека
+    char *stack_p; // указатель на стек
+    int top; // номер текущего элемента стека
     const int size_s = 10;
 public:
     my_stack() { // конструктор
-        stack_p = new my_stack[size_s];
+        stack_p = new char[size_s];
         top = 0;
     };
     ~my_stack(){ // деструктор
         delete [] stack_p;
     };
+    void push(char &); // поместить элемент на вершину стека
     void print_s(); // вывод стека
-    void push(my_stack &); // поместить элемент на вершину стека
 };
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     my_stack test;
+    char ch = '2';
+    test.push(ch);
     test.print_s();
     return a.exec();
+}
+
+void my_stack::push(char &value)
+{
+    if (top < size_s) // Пока так
+        stack_p[top++] = value; // помещаем элемент в стек
+    else
+        cout << "Stack is full";
 }
 
 void my_stack::print_s()
 {
     for (int i = top - 1; i >= 0; i--)
-        cout << " | " << &stack_p[i] << endl;
+        cout << " | " << stack_p[i] << endl;
 }
